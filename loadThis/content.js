8d3810951fe03f695b0922802415019bfbcb331d
Hans <hansmandersingh1998@gmail.com>
2020-05-17 14:26:19 -0500

    Fixes rating where link was not clickable

--- loadThis/content.js
+++ loadThis/content.js
@@ -2,12 +2,13 @@ const fetchRatings = () => {
   let mediaList = document.querySelectorAll('.media-lists__td-title');
 
   mediaList.forEach(movie => {
-    let movieName = movie.innerText;
+    let actualMovieNAme = movie.querySelector('a');
+    let movieName = actualMovieNAme.innerText;
 
     fetch(`https://www.omdbapi.com/?t=${movieName}&apikey=e7468b07`)
     .then(data => data.json())
     .then(json => {
-      movie.innerText = `${movieName} (${json.imdbRating})`;
+      actualMovieNAme.innerText = `${movieName} (${json.imdbRating})`;
     })
   })
 }
