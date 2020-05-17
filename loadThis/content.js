b4aae00c12fd31c8bb862a044256c720def5cf4e
Hans <hansmandersingh1998@gmail.com>
2020-05-17 13:50:35 -0500

    gets all the movie name being displayed right now on the webpage

---
+++ loadThis/content.js
@@ -0,0 +1,11 @@
+const fetchRatings = () => {
+  let mediaList = document.querySelectorAll('.media-lists__td-title');
+
+  mediaList.forEach(movie => {
+    let movieName = movie.innerText;
+
+    
+  })
+}
+
+fetchRatings();
