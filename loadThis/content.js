d34503c17c0200d8c1e3db4cc0a09f33221133fa
Hans <61017041+hansmandersingh@users.noreply.github.com>
2020-10-29 10:24:07 -0500

    Update content.js
    Some site changes and fixes added

--- loadThis/content.js
+++ loadThis/content.js
@@ -1,16 +1,32 @@
 function fetchRatings() {
-  let mediaList = document.querySelectorAll('.media-lists__td-title');
-
+  let mediaList = document.querySelectorAll('.dynamic-text-list__item-title');
+  let smallTitles = document.querySelectorAll('.p--small');
+  
   mediaList.forEach(movie => {
-    let actualMovieNAme = movie.querySelector('a');
-    let movieName = actualMovieNAme.innerText;
-
+    
+    let movieName = movie.innerText;
+    
     fetch(`https://www.omdbapi.com/?t=${movieName}&apikey=e7468b07`)
     .then(data => data.json())
     .then(json => {
-      actualMovieNAme.innerText = `${movieName} (${json.imdbRating})`;
+      if(json.imdbRating !== undefined) {
+        movie.innerText = `${movieName} (${json.imdbRating})`;
+      }
     })
   })
+
+  smallTitles.forEach(movie => {
+    let movieName = movie.innerText;
+    
+    fetch(`https://www.omdbapi.com/?t=${movieName}&apikey=e7468b07`)
+    .then(data => data.json())
+    .then(json => {
+      if(json.imdbRating !== undefined) {
+        movie.innerText = `${movieName} (${json.imdbRating})`;
+      }
+    })
+  })
+  
 }
 
 
